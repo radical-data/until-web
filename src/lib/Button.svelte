0085a32063b8ad16665b63619e08b996aa9d2001
Jo Kroese <jo@jokroese.com>
2023-12-20 13:10:32 +0100

    create "go to" button

---
+++ src/lib/Button.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let href: string;
+	import arrow from '$lib/assets/right-arrow.svg';
+</script>
+
+<a {href}>
+	<slot />
+    <img src={arrow} alt="">
+</a>
+
+<style>
+	a {
+		padding: 0.625rem;
+		justify-content: center;
+		align-items: center;
+		gap: 0.625rem;
+		border: 1px solid #f4f4f4;
+		border-radius: 9999rem;
+		display: inline-flex;
+        text-decoration: none;
+	}
+</style>
