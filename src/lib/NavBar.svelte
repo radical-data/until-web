ea0c5708363a17a9e3889c4285e759c2dcc85ab7
Jo Kroese <jo@jokroese.com>
2023-12-20 13:08:57 +0100

    add links to navbar

--- src/lib/NavBar.svelte
+++ src/lib/NavBar.svelte
@@ -1,32 +1,47 @@
 <nav>
 	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/database">Database</a></li>
+		<li><a href="/about">About</a></li>
+	</ul>
+	<ul>
 		<li><a href="/en">en</a></li>
 		<li><a href="/ar"><span class="arabic">العربية</span></a></li>
 	</ul>
 </nav>
 
 <style>
+	nav {
+	  display: flex;
+	  justify-content: space-between;
+	  align-items: center;
+	}
+  
 	ul {
-		list-style: none;
-		padding: 0;
-		margin: 0;
-		display: flex;
-		justify-content: flex-end;
+	  list-style: none;
+	  padding: 0;
+	  margin: 0;
+	  display: flex;
 	}
-
+  
+	ul:first-child {
+	  margin-right: auto; /* Pushes the first ul to the left */
+	}
+  
 	a {
-		text-decoration: none;
-		color: white;
-		display: block;
-		padding: 0px 10px;
-		margin: 0;
-		transition: all 0.3s ease;
-		border-radius: 9999px;
-		font-weight: 600;
+	  text-decoration: none;
+	  color: white;
+	  display: block;
+	  padding: 0px 10px;
+	  margin: 0;
+	  transition: all 0.3s ease;
+	  border-radius: 9999px;
+	  font-weight: 600;
 	}
-
+  
 	a:hover {
-		background-color: #f4f4f4;
-		color: #101010;
+	  background-color: #f4f4f4;
+	  color: #101010;
 	}
-</style>
+  </style>
+  
