be4d0cb252e610f8b6d7e53581e69a5ece308114
Jo Kroese <jo@jokroese.com>
2023-12-20 13:09:25 +0100

    add about page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,86 @@
+<script>
+	import LastUpdated from '$lib/LastUpdated.svelte';
+</script>
+
+<h1><span class="arabic">حول</span><br />About</h1>
+<p class="subtitle">
+	Until is a community-led database of Palestinians killed in the Israeli assault on Gaza.
+</p>
+<main>
+	<article>
+		<section>
+			<p>
+				The ongoing genocide seeks to dehumanise the Palestinian people by reducing them to numbers,
+				anonymous bodies and mass graves.
+			</p>
+			<p>
+				<span class="until">Until</span> is a community effort to collect names, images and stories of
+				the people who have been killed. The database gives name, singularity, and humanity to each person,
+				showing that each death in these statistics are people with lives, loves and joy.
+			</p>
+		</section>
+		<section>
+			<h2>Contributing</h2>
+			<p>
+				As a community project, <span class="until">Until</span> is built by volunteers like you. There
+				are 2 main ways you can support:
+			</p>
+			<ol>
+				<li>
+					<span class="focus">Submit sources</span>: Use the
+					<a href="source-submission">source submission form</a> to share sources (e.g. news articles,
+					social media updates). These will be integrated into the database.
+				</li>
+				<li>
+					<span class="focus">Share</span>: To be useful, <span class="until">Until</span> needs to be
+					used. Please share it with your family, friends and representatives.
+				</li>
+			</ol>
+			<p>
+				If you have other skills that could be helpful (design, translation, programming, social
+				media support etc.), please get in touch through the <a href="contribution-form"
+					>contribution form</a
+				>.
+			</p>
+		</section>
+		<section>
+			<h2>Methodology</h2>
+			<p>
+				uses an iterative approach to build the database. People can add sources which are sorted
+				through a hybrid process of automation and moderation. Through this, we combine information
+				from various sources to build as detailed picture as possible.
+			</p>
+			<p>
+				We also invite testimonials and images to give a more complete picture of each of the people
+				in the database.
+			</p>
+		</section>
+		<section>
+			<h2>Who we are</h2>
+			<p>
+				<span class="until">Until</span> is a community project led by Radical Data with friends and
+				volunteers.
+			</p>
+			<p>
+				The project started a few weeks ago as a way to not be passive while people’s agency is
+				taken away. This is an ongoing process of learning.
+			</p>
+			<p>
+				We started this with experience of building similar databases of mass killings in the past,
+				particularly during the dictatorships of Chile and Argentina. Usually, our projects are
+				built collaboratively with the community they are about. Due to internet shutdowns, we are
+				learning how to do this with a diverse collective. This is the first time we are using the
+				approach in a crisis in the present. That has some challenges and we're learning how to deal
+				with them as we go.
+			</p>
+		</section>
+	</article>
+</main>
+
+<style>
+	.last-updated {
+		display: flex;
+		justify-content: center;
+		margin-top: 1rem;
+	}
+</style>
