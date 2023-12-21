6dc13fd225bd73bddc8ee0a3aadf7e3c383640eb
Jo Kroese <jo@jokroese.com>
2023-12-21 17:29:27 +0100

    use already fetched data

--- src/routes/database/[slug]/+page.server.ts
+++ src/routes/database/[slug]/+page.server.ts
@@ -1,10 +1,10 @@
 import { getDeathById } from "$lib/fetchById";
-import { fetchDeathsData } from "$lib/fetchData.js";
+import { deaths } from "$lib/stores.js";
 import { error } from "@sveltejs/kit";
+import { get } from "svelte/store";
 
 export async function load({ params }) {
-    const deathsData = await fetchDeathsData();
-    const post = await getDeathById(params.slug, deathsData)
+    const post = await getDeathById(params.slug, get(deaths))
     if (!post) {
         throw error(404, 'Page Not Found');
     }
