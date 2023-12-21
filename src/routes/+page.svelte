e68fc54f3c13171dce207181ecceec5f42c08986
Jo Kroese <jo@jokroese.com>
2023-12-21 12:56:17 +0100

    update home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,39 @@
 <script>
+	import Button from '$lib/Button.svelte';
 	import LastUpdated from '$lib/LastUpdated.svelte';
+	import { deaths } from '$lib/stores';
+	import { getRandomSubset, toKebabCase } from '$lib/utils';
+	$: deathsToShow = getRandomSubset($deaths, 30);
+	let totalDeathEstimate = 19667;
 </script>
 
-<h1><span class="arabic">إلى أن</span><br />Until</h1>
-<p class="subtitle">A community-led database of Palestinians killed in Israeli assault on Gaza.</p>
+<svelte:head>
+	<title>Until · A community database of killings in Gaza</title>
+	<meta name="description" content="Until: A community-led database of Palestinians killed in the Israeli assault on Gaza. This database humanizes each individual beyond numbers, offering names, images, and stories. Contribute by submitting sources, images or testimonials." />
+</svelte:head>
+
+<h1><span class="arabic">ريثما</span><br />Until</h1>
+<p class="subtitle">
+	A community-led database of Palestinians killed in the Israeli assault on Gaza.
+</p>
 <main>
+	<div class="last-updated">
+		<LastUpdated lastUpdatedTime={new Date('2023-11-27T15:43')}></LastUpdated>
+	</div>
 	<article>
 		<section>
-			<p>
-				On <time datetime="2023-11-10">November 10th</time>, Gaza City's health system collapsed.
-				Since then, the Ministry of Health in Gaza has not been able to provide regular casualty
-				figures.
-			</p>
-			<p>
-				<span class="until">Until</span> is a community effort to fill that space. It provides a regularly-updated
-				database of who is being killed in the Israel-Hamas War.
-			</p>
-		</section>
-		<section>
-			<div class="last-updated">
-				<LastUpdated lastUpdatedTime={new Date('2023-11-27T15:43')}></LastUpdated>
-			</div>
 			<p class="statement">
-				<span class="number">14,784</span> killed,<br />
-				<span class="number">6,432</span> of them children.
+				<span class="highlight">Until</span> remembers <span class="highlight">{new Intl.NumberFormat().format($deaths.length)}</span> of the
+				<span class="highlight">{new Intl.NumberFormat().format(totalDeathEstimate)}</span> killed.
 			</p>
-			<p class="invite">See the <a href="database">full database</a>.</p>
+			<p class="deaths-list">
+				{#each deathsToShow as death, index}
+					<a href={'database/' + toKebabCase(death.name_en)}>
+						<span class="name">{death.name_en}</span>, <span class="age">{death.age}</span></a
+					>{index !== deathsToShow.length - 1 ? ' · ' : ''}
+				{/each}
+			</p>
+			<p class="invite">For their stories, <Button href="/database">Go to database</Button></p>
 		</section>
 		<section>
 			<h2>Contributing</h2>
@@ -36,7 +44,7 @@
 			<ol>
 				<li>
 					<span class="focus">Submit sources</span>: Use the
-					<a href="source-submission">source submission form</a> to share sources (e.g. news articles,
+					<a href="https://wjtikx0v.nocodb.com/#/nc/form/5fd4112d-b5d0-487d-aaa2-9559710858c6">source submission form</a> to share sources (e.g. news articles,
 					social media updates). These will be integrated into the database.
 				</li>
 				<li>
@@ -46,14 +54,10 @@
 			</ol>
 			<p>
 				If you have other skills that could be helpful (design, translation, programming, social
-				media support etc.), please get in touch through the <a href="contribution-form"
+				media support etc.), please get in touch through the <a href="https://wjtikx0v.nocodb.com/#/nc/form/83775246-bd27-48f0-ba98-e604f9f5f79e"
 					>contribution form</a
 				>.
 			</p>
-		</section>
-		<section>
-			<h2>Methodology</h2>
-			<p><span class="until">Until</span> uses an iterative approach to build the database.</p>
 		</section>
 	</article>
 </main>
@@ -62,6 +66,17 @@
 	.last-updated {
 		display: flex;
 		justify-content: center;
-        margin-top: 1rem;
+		margin-top: 1rem;
+	}
+	.statement {
+		font-size: 1.75rem;
+		font-style: italic;
+		font-weight: 400;
+		/* padding-left: 1.5rem; */
+		text-align: center;
+	}
+
+	.statement .highlight {
+		font-weight: 700;
 	}
 </style>
